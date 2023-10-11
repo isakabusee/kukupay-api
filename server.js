1a9a0e311ad1ea6b34247cbbe0a09cc8bb349b6d
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2023-10-11 00:07:59 -0600

    updated cors settings

--- server.js
+++ server.js
@@ -1,47 +1,53 @@
-import express from 'express';
-import data from './data.js';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import seedRouter from './routes/seedRoutes.js';
-import productRouter from './routes/productRoutes.js';
-import userRouter from './routes/userRoutes.js';
-import orderRouter from './routes/orderRoutes.js';
-import cors from 'cors';
+import express from "express";
+import data from "./data.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import seedRouter from "./routes/seedRoutes.js";
+import productRouter from "./routes/productRoutes.js";
+import userRouter from "./routes/userRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
+import cors from "cors";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URI).then(()=>{
-    console.log('connected to db')
-}).catch(error => {
-    console.log(error.message)
-});
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
 
 const app = express();
 
-// cors
-app.use(cors({
-    origin: 'https://kukupay-ui-mquk2.ondigitalocean.app'
-  }));
-// to convert form data into json data in req.body 
+// to convert form data into json data in req.body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// cors
+app.use(cors());
+app.options("*", cors());
+// app.use(cors({
+//     origin: 'https://kukupay-ui-mquk2.ondigitalocean.app'
+//   }));
+
 // my APIs below
 
-app.get('/api/keys/paypal', (req, res) => {
-    res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+app.get("/api/keys/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
 });
 
-app.use('/api/seed', seedRouter);
-app.use('/api/products', productRouter);
-app.use('/api/users', userRouter);
-app.use('/api/orders', orderRouter);
+app.use("/api/seed", seedRouter);
+app.use("/api/products", productRouter);
+app.use("/api/users", userRouter);
+app.use("/api/orders", orderRouter);
 
-  
 app.use((err, req, res, next) => {
-    res.status(500).send({ message: err.message });
+  res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`serve at http://localhost:${port}`);
+  console.log(`serve at http://localhost:${port}`);
 });
