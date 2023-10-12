f0e908da38a3144f28eda0ae5f4d439c4c3b5355
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2023-10-12 00:40:55 -0600

    cors import

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import seedRouter from "./routes/seedRoutes.js";
 import productRouter from "./routes/productRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import orderRouter from "./routes/orderRoutes.js";
-// import cors from "cors";
+import cors from "cors";
 
 dotenv.config();
 
