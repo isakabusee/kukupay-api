e81e81822252423070d5233160ca98e66efb71c1
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2023-10-12 00:20:50 -0600

    log for products

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,6 +5,7 @@ const productRouter = express.Router();
 
 productRouter.get("/", async (req, res) => {
   const products = await Product.find();
+  console.log(products, "products");
   res.send(products);
 });
 
@@ -17,7 +18,7 @@ productRouter.get("/slug/:slug", async (req, res) => {
   }
 });
 productRouter.get("/:id", async (req, res) => {
-  const product =  await Product.findById(req.params.id);
+  const product = await Product.findById(req.params.id);
   if (product) {
     res.send(product);
   } else {
